9be7d638c94c3705123ac6244f4c1e84fb550229
BlackHeartNet <juniormarketingfc@gmail.com>
2024-10-03 13:58:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-let votes = {
-    "Opção 1": 0,
-    "Opção 2": 0,
-    "Opção 3": 0,
+let votos = {
+    "TED E MARCELO": 0,
+    "DÉ CARAJE E WAGNER": 0,
+    "MARCONI E LUANDER": 0
 };
 
-function vote(option) {
-    votes[option]++;
-    displayResults();
+function vote(opcao) {
+    votos[opcao]++;
+    atualizarResultados();
 }
 
-function displayResults() {
+function atualizarResultados() {
     const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = `
-        <h3>Resultados:</h3>
-        <p>Opção 1: TED E MARCELO - ${votes["Opção 1"]} votos</p>
-        <p>Opção 2: DÉ CARAJE E WAGNER - ${votes["Opção 2"]} votos</p>
-        <p>Opção 3: MARCONI E LUANDER - ${votes["Opção 3"]} votos</p>
-    `;
+    resultDiv.innerHTML = '<h3>Resultados:</h3>' +
+        `<p>TED E MARCELO - ${votos["TED E MARCELO"]} votos</p>` +
+        `<p>DÉ CARAJE E WAGNER - ${votos["DÉ CARAJE E WAGNER"]} votos</p>` +
+        `<p>MARCONI E LUANDER - ${votos["MARCONI E LUANDER"]} votos</p>`;
+}
+
+function toggleVoteButton() {
+    const checkbox = document.getElementById('termos');
+    const voteSection = document.getElementById('votacao');
+    if (checkbox.checked) {
+        voteSection.style.display = 'block';
+    } else {
+        voteSection.style.display = 'none';
+    }
 }
